23ef014dbcda8746516e794096be510886390324
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-05-12 02:29:39 +0200

    add assets

---
+++ lib/utils/app_assets.dart
@@ -0,0 +1,28 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesOnBoardringOne
+  /// assets/images/on-boardring-one.jpg
+  static const String imagesOnBoardringOne = "assets/images/on-boardring-one.jpg";
+
+  /// Assets for imagesOnBoardringThree
+  /// assets/images/on-boardring-three.jpg
+  static const String imagesOnBoardringThree = "assets/images/on-boardring-three.jpg";
+
+  /// Assets for imagesOnBoardringTwo
+  /// assets/images/on-boardring-two.jpg
+  static const String imagesOnBoardringTwo = "assets/images/on-boardring-two.jpg";
+
+  /// Assets for imagesProviderAuth
+  /// assets/images/provider-auth.png
+  static const String imagesProviderAuth = "assets/images/provider-auth.png";
+
+  /// Assets for imagesWelcomeOne
+  /// assets/images/welcome-one.jpg
+  static const String imagesWelcomeOne = "assets/images/welcome-one.jpg";
+
+  /// Assets for imagesWelcomeTwo
+  /// assets/images/welcome-two.jpg
+  static const String imagesWelcomeTwo = "assets/images/welcome-two.jpg";
+}
+
