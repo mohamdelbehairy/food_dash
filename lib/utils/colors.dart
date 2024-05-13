bfce0dccb7e6705f90180836c9baf1052736c0a6
Fatma-Alzhraa-Esmail <fatmaalzhraazedan@gmail.com>
2024-05-14 02:51:38 +0300

    colors added

---
+++ lib/utils/colors.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+abstract class AppColors{
+static const mainColor = Color(0XFF29974D);
+static const textFieldFillColor = Color(0XFFFAFAFA);
+static const textFieldFocusColor = Color(0XFFC2E8D8);
+static const textFieldHintColor = Color(0XFFD4D3D2);
+static const primaryColor = Color(0XFF000000);
+static const secondaryColor = Color(0XFFB9B8B9); //LIKE CONTINUE WITH
+static const pinkColor = Color(0XFFF79CA5);
+static const appBarTitleColor = Color(0XFF76885D);
+
+//Edit Profile
+// const pinBackground = Color(0XFFD4D3D2);
+static const pinFocusBackground = Color(0XFFEDF8F0);
+static const rateColor = Color(0XFFF9AA37);
+// const inactiveBtnColor = Color(0XFFD4D3D2);
+// const activeBtnColor = Color(0XFFD4D3D2);
+// const textFieldBC = Color(0XFFD4D3D2);
+// const redColor = Color(0XFFFC6276);
+ //bottom sheet colors
+
+static const documentColor = Color(0XFF377BFB);
+static const cameraColor = Color(0XFFFC6276);
+static const galleryColor = Color(0XFFFCA01D);
+static const audioColor = Color(0XFF9E29AC);
+static const locationColor = Color(0XFF26B656);
+static const contactColor = Color(0XFF6239B0);
+}
