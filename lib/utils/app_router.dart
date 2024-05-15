e0482b197e788f939062f423587691f846b4d226
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-05-15 13:34:36 +0200

    create app router class

---
+++ lib/utils/app_router.dart
@@ -0,0 +1,8 @@
+import 'package:food_dash/features/splash/ui/views/splash_view.dart';
+import 'package:go_router/go_router.dart';
+
+abstract class AppRouter {
+  static final router = GoRouter(routes: [
+    GoRoute(path: '/', builder: (context, state) => const SplashView()),
+  ]);
+}
