ecb10ede8938ff12d55266b78ccb633ef1267c9f
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-05-23 22:50:07 +0200

    create bloc observer class

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:food_dash/bloc_observer.dart';
 import 'package:food_dash/core/utils/app_router.dart';
 import 'package:food_dash/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = SimpleBlocObserver();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
