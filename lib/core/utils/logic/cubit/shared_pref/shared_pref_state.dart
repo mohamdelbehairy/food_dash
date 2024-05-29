e081932903e49eeab421c17e25b5a702c91996fa
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-05-29 07:29:45 +0200

    shared prefrences to login in app done

--- lib/core/utils/logic/cubit/shared_pref/shared_pref_state.dart
+++
@@ -1,8 +0,0 @@
-part of 'shared_pref_cubit.dart';
-
-
-sealed class SharedPrefState {}
-
-final class SharedPrefInitial extends SharedPrefState {}
-
-final class UserSharedPrefSuccess extends SharedPrefState {}
